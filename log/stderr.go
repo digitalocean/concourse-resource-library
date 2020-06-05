a509bf84782d0323388600d25a6264aa098c964c
Christopher Mancini <cmancini@digitalocean.com>
2020-06-05 09:26:49 -0400

    add new line to stderr output

--- log/stderr.go
+++ log/stderr.go
@@ -8,5 +8,5 @@ import (
 // StdErr outputs a message & the string form of a data structure (`v`) to the stderr stream
 // 	this is helpful, as Concourse consumes the `stdout` stream for metadata
 func StdErr(msg string, v interface{}) {
-	fmt.Fprintf(os.Stderr, "%s: %s", msg, v)
+	fmt.Fprintf(os.Stderr, "%s: %s\n", msg, v)
 }
