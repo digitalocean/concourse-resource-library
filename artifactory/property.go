9f6c85db7175677ee04c9fa83af403bfff54c601
Christopher Mancini <cmancini@digitalocean.com>
2020-06-09 15:48:25 -0400

    artifactory: properties / env from file

--- artifactory/property.go
+++ artifactory/property.go
@@ -1,5 +1,13 @@
 package artifactory
 
+import (
+	"bufio"
+	"os"
+	"strings"
+
+	"github.com/jfrog/jfrog-client-go/artifactory/buildinfo"
+)
+
 // Property stores an artifact metadata property field / value pair
 type Property struct {
 	Name  string
@@ -20,4 +28,34 @@ func (props Properties) String() string {
 	}
 
 	return s
+}
+
+// FromFile reads a simple kv file into Properties
+func (props *Properties) FromFile(p string) error {
+	f, err := os.Open(p)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		kv := strings.SplitN(scanner.Text(), "=", 2)
+		if len(kv) == 2 {
+			*props = append(*props, Property{Name: kv[0], Value: strings.Trim(kv[1], `'"`)})
+		}
+	}
+
+	return nil
+}
+
+// Env converts Properties to Artifactory buildinfo.Env
+func (props Properties) Env() buildinfo.Env {
+	e := buildinfo.Env{}
+
+	for _, p := range props {
+		e[p.Name] = p.Value
+	}
+
+	return e
 }
