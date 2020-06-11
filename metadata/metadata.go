2ca0343261f68fb80b7a34832f2e77b0892beb79
Christopher Mancini <cmancini@digitalocean.com>
2020-06-11 17:16:33 -0400

    add default dir for macos & improve metadata functionality

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -3,6 +3,8 @@ package metadata
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"path/filepath"
 )
 
 // Metadata output from get/put steps
@@ -12,11 +14,31 @@ type Metadata []*Field
 type Field struct {
 	Name  string `json:"name"`
 	Value string `json:"value"`
+	JSON  bool   `json:"-"`
+}
+
+// FileName returns filename for metadata file
+func (f *Field) FileName() string {
+	if f.JSON {
+		return f.Name + ".json"
+	}
+
+	return f.Name
+}
+
+// Content returns filename for metadata file
+func (f *Field) Content() []byte {
+	return []byte(f.Value)
 }
 
 // Add a Field to the Metadata
 func (m *Metadata) Add(name string, value interface{}) {
 	*m = append(*m, &Field{Name: name, Value: fmt.Sprintf("%s", value)})
+}
+
+// AddJSON a Field to the Metadata
+func (m *Metadata) AddJSON(name string, value interface{}) {
+	*m = append(*m, &Field{Name: name, Value: fmt.Sprintf("%s", value), JSON: true})
 }
 
 // Get a Field of the Metadata
@@ -33,4 +55,15 @@ func (m *Metadata) Get(name string) string {
 // JSON returns an encoded byte slice of an Artifact
 func (m *Metadata) JSON() ([]byte, error) {
 	return json.Marshal(m)
+}
+
+// ToFiles writes metadata to files on disk as `resource/{Name}` with content {Value}
+func (m Metadata) ToFiles(path string) error {
+	for _, d := range m {
+		if err := ioutil.WriteFile(filepath.Join(path, d.FileName()), d.Content(), 0644); err != nil {
+			return fmt.Errorf("failed to write metadata file %s: %s", d.FileName(), err)
+		}
+	}
+
+	return nil
 }
